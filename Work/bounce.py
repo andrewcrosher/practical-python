9187005ca5f05496e88d87b7b1c8274d47dbc2c2
Andrew Crosher <andrewcrosher@outlook.com>
2020-10-21 21:43:54 +0100

    added work from exercise 1.2

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bounceRatio = 0.6
+bounceN = 1
+
+while bounceN < 11:
+    height = height * bounceRatio
+    print(bounceN, round(height,4))
+    bounceN = bounceN + 1
